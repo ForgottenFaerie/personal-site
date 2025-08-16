c1b9c67612313edf69c7d5a7613a3ea897b5da1e
Kenny Thomas-Moore <220403301+ForgottenFaerie@users.noreply.github.com>
2025-08-15 22:01:52 -0400

    Add styles (mobile/desktop), add basic hero section on index.php & favicon

--- src/index.php
+++ src/index.php
@@ -1,3 +1,9 @@
+<?php
+
+
+
+?>
+
 <!DOCTYPE HTML>
 <html>
     <head>
@@ -6,12 +12,52 @@
         <link rel="stylesheet" href="styles.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charset="utf-8" />
+        <meta lang="en" />
+        <meta name="description" content="Welcome to nonbinarybyte.com, the personal website of Forgotten Faerie, a nonbinary software developer and artist. Explore my portfolio, blog, and more." />
+        <meta name="keywords" content="nonbinary, software developer, artist, personal website, portfolio, blog" />
+        <meta name="author" content="Forgotten Faerie" />
+        <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cal+Sans&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inconsolata:wght@200..900&family=Lexend+Deca:wght@100..900&family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
     </head>
     <body>
         <header>
             <div class="HDR-Col-1">
                 <h1>nonbinarybyte.com</h1>
             </div>
+            <div class="HDR-Col-2">
+                <nav>
+                    <button>
+                        <a href="index.php">Home.</a>
+                    </button>
+                    <button>
+                        <a href="blog.php">Blog.</a>
+                    </button>
+                    <button>
+                        <a href="portfolio.php">Portfolio.</a>
+                    </button>
+                    <button>
+                        <a href="https://github.com/ForgottenFaerie/personal-site">Website Source Code.</a>
+                    </button>
+                    <button>
+                        <a href="https://shop.nonbinarybyte.com">My Store.</a>
+                    </button>
+                </nav>
+            </div>
         </header>
+        <main>
+            <article>
+                <section class="hero">
+                    <div class="HERO-img">
+                        <img alt="picture of me!" src="https://github.com/ForgottenFaerie/nonbinarybyte.com/blob/main/assets/profile-pic2.png?raw=true" width="155" height="155" />
+                    </div>
+                    <div class="HERO-txt">
+                        <h2>Hi, I'm Kenny!</h2>
+                        <p>I am a Web Developer, & Artist.</p>
+                    </div>
+                </section>
+            </article>
+        </main>
     </body>
 </html>
